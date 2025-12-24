0385317f7e361548e2139e0d243b08a4c3dbe180
AhsanDeveloper764 <ahsanalise226@gmail.com>
2025-12-25 02:48:20 +0500

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// const express = require("express")
-// const cors = require("cors")
-// const dbConnect = require("./db/index")
-// const {PORT} = require("./config/index")
-// const router = require("./routes/index")
-// const cookieParser   = require("cookie-parser");
-// const errorHandler = require("./middlewares/errorHandle")
+const express = require("express")
+const cors = require("cors")
+const dbConnect = require("./db/index")
+const {PORT} = require("./config/index")
+const router = require("./routes/index")
+const cookieParser   = require("cookie-parser");
+const errorHandler = require("./middlewares/errorHandle")
 
 // const app = express();
 // app.use(cookieParser());
@@ -29,49 +29,30 @@
 // // isko humnay end ma isliye registered krwaya hay ku kay jintay bhi middlewares hotay hein wo sequentially run hotay hein
 // app.listen(PORT, console.log(`Backend is running on port: ${PORT}`));
 
-const express = require("express");
-const cors = require("cors");
-const dbConnect = require("./db/index");
-const { PORT } = require("./config/index");
-const router = require("./routes/index");
-const cookieParser = require("cookie-parser");
-const errorHandler = require("./middlewares/errorHandle");
-
 const app = express();
 
 app.use(cookieParser());
 
-// ✅ SIMPLE + WORKING CORS
+// app.use(cors(corsOptions));
+
 app.use(
   cors({
-    origin: "https://coin-frontend-app.onrender.com",
-    credentials: true,
-  })
-);
-
-// ✅ Preflight fix
-app.options(
-  "*",
-  cors({
-    origin: "https://coin-frontend-app.onrender.com",
+    origin: function (origin, callback) {
+      return callback(null, true);
+    },
+    optionsSuccessStatus: 200,
     credentials: true,
   })
 );
 
 app.use(express.json({ limit: "50mb" }));
-app.use("/storage", express.static("storage"));
+
 app.use(router);
 
 dbConnect();
 
+app.use("/storage", express.static("storage"));
+
 app.use(errorHandler);
 
-app.listen(PORT, () =>
-  console.log(`Backend is running on port ${PORT}`)
-);
-
-
-// const corsOption = {
-//     credentials:true,
-//     origin:['http://localhost:5173']
-// }
+app.listen(PORT, console.log(`Backend is running on port: ${PORT}`));
