5f6d157b6be9fd9e338bc4ffef83c2c100868b6f
AhsanDeveloper764 <ahsanalise226@gmail.com>
2025-12-25 03:09:40 +0500

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const cors = require("cors")
-const dbConnect = require("./db/index")
-const {PORT} = require("./config/index")
-const router = require("./routes/index")
-const cookieParser   = require("cookie-parser");
-const errorHandler = require("./middlewares/errorHandle")
+// const express = require("express")
+// const cors = require("cors")
+// const dbConnect = require("./db/index")
+// const {PORT} = require("./config/index")
+// const router = require("./routes/index")
+// const cookieParser   = require("cookie-parser");
+// const errorHandler = require("./middlewares/errorHandle")
 
 // const app = express();
 // app.use(cookieParser());
@@ -29,13 +29,23 @@ const errorHandler = require("./middlewares/errorHandle")
 // // isko humnay end ma isliye registered krwaya hay ku kay jintay bhi middlewares hotay hein wo sequentially run hotay hein
 // app.listen(PORT, console.log(`Backend is running on port: ${PORT}`));
 
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dbConnect = require("./db/index");
+const router = require("./routes/index");
+const {PORT} = require("./config/index")
+const errorHandler = require("./middlewares/errorHandle");
+
 const app = express();
-app.use(cookieParser());
-// app.use(cors(corsOptions));
+
 const allowedOrigins = [
   "https://coin-frontend-app.onrender.com",
   "http://localhost:5173"
 ];
+
+app.use(cookieParser());
 
 app.use(
   cors({
@@ -43,20 +53,24 @@ app.use(
       if (!origin) return callback(null, true);
 
       if (allowedOrigins.includes(origin)) {
-        callback(null, origin);
-      } else {
-        callback(new Error("Not allowed by CORS"));
+        return callback(null, origin);
       }
+
+      return callback(new Error("CORS not allowed"));
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
+
 app.use(express.json({ limit: "50mb" }));
+
 app.use(router);
+
 dbConnect();
+
 app.use("/storage", express.static("storage"));
+
 app.use(errorHandler);
 app.listen(PORT, console.log(`Backend is running on port: ${PORT}`));
